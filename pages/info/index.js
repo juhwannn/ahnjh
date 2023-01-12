c0194c8bea6ff0ae9caae650322e9a23a92f03b1
ahnjh <algorithm123@naver.com>
2023-01-12 15:20:29 +0900

    init

---
+++ pages/info/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {useRouter} from "next/router";
+
+
+export default function Home() {
+    const router = useRouter();
+
+    return (
+        <div>
+            asdf
+        </div>
+    );
+}
